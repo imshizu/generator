b44f14cd6b609b3766303efcc04a3fe687783e98
shizu <mikkelelvers@outlook.com>
2024-10-20 00:08:15 +0200

    added a way to extract tiers from itemstacks

--- src/main/java/eu/altshizu/generator/utils/Item.java
+++ src/main/java/eu/altshizu/generator/utils/Item.java
@@ -1,2 +1,22 @@
-package eu.altshizu.generator.utils;public class ItemStack {
+package eu.altshizu.generator.utils;
+
+import org.bukkit.inventory.ItemStack;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Item {
+
+    public static int getTier(ItemStack item) {
+        if(!item.hasItemMeta() && !item.getItemMeta().hasDisplayName()) return -1;
+        String name = item.getItemMeta().getDisplayName();
+
+        Pattern pattern = Pattern.compile("Tier (\\d+)");
+        Matcher matcher = pattern.matcher(name);
+
+        if(matcher.find()) {
+            return Integer.parseInt(matcher.group(1));
+        }
+        return -1;
+    }
 }
