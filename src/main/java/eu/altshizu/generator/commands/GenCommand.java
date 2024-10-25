10da1c559006679f2b4107f2307ae8c155fa8d56
Mikkel <mikkelelvers@outlook.com>
2024-10-25 22:53:22 +0200

    give gen implemented
    added give generator to gen command

--- src/main/java/eu/altshizu/generator/commands/GenCommand.java
+++ src/main/java/eu/altshizu/generator/commands/GenCommand.java
@@ -2,13 +2,12 @@ package eu.altshizu.generator.commands;
 
 import eu.altshizu.generator.configs.GensConfig;
 import eu.altshizu.generator.configs.LangConfig;
-import eu.okaeri.commands.annotation.Command;
-import eu.okaeri.commands.annotation.Context;
-import eu.okaeri.commands.annotation.Executor;
+import eu.okaeri.commands.annotation.*;
 import eu.okaeri.commands.bukkit.annotation.Async;
 import eu.okaeri.commands.bukkit.annotation.Permission;
 import eu.okaeri.commands.bukkit.annotation.Sync;
 import eu.okaeri.commands.service.CommandService;
+import eu.okaeri.commands.service.Option;
 import eu.okaeri.injector.annotation.Inject;
 import eu.okaeri.platform.bukkit.i18n.BI18n;
 import org.bukkit.entity.Player;
@@ -36,6 +35,35 @@ public class GenCommand implements CommandService {
         i18n.get(langConfig.getReloadEnd())
                 .with("prefix", i18n.get(player, langConfig.getPrefix()).apply())
                 .with("ms", System.currentTimeMillis() - ms)
+                .sendTo(player);
+    }
+
+    @Permission("generator.admin")
+    @Executor(pattern = "give * ?") @Sync
+    @Completion(arg = "target", value = "@bukkit:player:name")
+    public void give(@Context Player player, @Arg("tier") int tier, @Arg("target") Option<Player> target) {
+        Player targetPlayer = target.orElse(player);
+        if(gensConfig.getGenerators().get(tier) == null) {
+            i18n.get(langConfig.getGeneratorNotFound())
+                    .with("prefix", i18n.get(player, langConfig.getPrefix()).apply())
+                    .sendTo(player);
+            return;
+        }
+
+        targetPlayer.getInventory().addItem(gensConfig.getGenerators().get(tier).getItem());
+        i18n.get(langConfig.getReceiveGenerator())
+                .with("prefix", i18n.get(player, langConfig.getPrefix()).apply())
+                .with("tier", tier)
+                .sendTo(targetPlayer);
+        /*
+            Detect if targetPlayer is the same as
+            player, if not send a message to the player
+         */
+        if(targetPlayer == player) return;
+        i18n.get(langConfig.getGaveGenerator())
+                .with("prefix", i18n.get(player, langConfig.getPrefix()).apply())
+                .with("tier", tier)
+                .with("target", targetPlayer.getName())
                 .sendTo(player);
     }
 }
