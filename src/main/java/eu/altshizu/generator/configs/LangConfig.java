a425a17842820381ac05b34652485a2490184051
Mikkel <mikkelelvers@outlook.com>
2024-10-25 22:00:33 +0200

    Update LangConfig
    Remade so prefix is easy customizable

--- src/main/java/eu/altshizu/generator/configs/LangConfig.java
+++ src/main/java/eu/altshizu/generator/configs/LangConfig.java
@@ -13,13 +13,14 @@ import lombok.Getter;
 @SuppressWarnings("FieldMayBeFinal")
 @Messages(path = "lang", defaultLocale = "dk", provider = YamlSnakeYamlConfigurer.class)
 public class LangConfig extends LocaleConfig {
-    private String noPermission = "&6[Generator] &7Du har ikke adgang til denne kommando.";
+    private String prefix = "&6[Generator]";
+    private String noPermission = "{prefix} &7Du har ikke adgang til denne kommando.";
 
     /**
      * Default & default admin message for the plugin is stored here.
      */
     private String defaultMessage =
-            "&6[Generator]\n" +
+            "{prefix}\n" +
             "&7/gen list &fviser alle generators\n" +
             "&7/gen shop &fåbner shop med generators\n";
 
@@ -32,32 +33,32 @@ public class LangConfig extends LocaleConfig {
     /**
      * Messages for the generator command is stored here.
      */
-    private String addGenerator = "&6[Generator] &7Du tilføjede en ny generator. (Tier {tier})";
-    private String holdItem = "&6[Generator] &7Du skal holde et item i hånden.";
-    private String removeGenerator = "&6[Generator] &7Du fjernede &e{tier} &7generator.";
-    private String generatorNotFound = "&6[Generator] &7Denne generator findes ikke.";
-    private String receiveGenerator = "&6[Generator] Du modtog &e{tier} &7generator.";
+    private String addGenerator = "{prefix} &7Du tilføjede en ny generator. (Tier {tier})";
+    private String holdItem = "{prefix} &7Du skal holde et item i hånden.";
+    private String removeGenerator = "{prefix} &7Du fjernede &e{tier} &7generator.";
+    private String generatorNotFound = "{prefix} &7Denne generator findes ikke.";
+    private String receiveGenerator = "{prefix} Du modtog &e{tier} &7generator.";
 
     /**
      * Messages for the generator shop is stored here.
      */
-    private String buyGenerator = "&6[Generator] &7Du købte en &etier {tier} generator&7.";
-    private String notEnoughMoney = "&6[Generator] &7Du mangler &c${money}&7.";
+    private String buyGenerator = "{prefix} &7Du købte en &etier {tier} generator&7.";
+    private String notEnoughMoney = "{prefix} &7Du mangler &c${money}&7.";
 
     /**
      * Messages for the generator events is stored here.
      */
-    private String placedSellchest = "&6[Generator] &7Du placerede en &esælgekiste&7.";
-    private String addedToStack = "&6[Generator] &7Du tilføjede en generator til stacken. ({amount} i alt)";
-    private String placedGenerator = "&6[Generator] &7Du placerede en &etier {tier} generator&7.";
-    private String maxGenerators = "&6[Generator] &7Du har ikke plads til flere generators.";
-    private String sellSuccess = "&6[Generator] &7Du indsamlet &a${money} &7& &b{xp} XP&7.";
-    private String sellFail = "&6[Generator] &7Der er ikke noget i denne sælgekiste.";
-    private String upgradeSuccess = "&6[Generator] &7Du opgraderet &e{amount}x tier {tier} generator&7 for &a${money}&7.";
-    private String upgradeFail = "&6[Generator] &7Du mangler &c${money}&7.";
-    private String upgradeMax = "&6[Generator] &7Du har den bedste generator tier.";
-    private String notYourGenerator = "&6[Generator] &7Du ejer ikke denne generator. ({owner})";
-    private String removeFromStack = "&6[Generator] &7Du fjernede &e{amount}x generator &7fra stacken. &7({gens} i alt)";
-    private String notEnoughInventorySpace = "&6[Generator] &7Du har ikke nok inventory plads.";
-    private String removeStack = "&6[Generator] &7Du fjernede en generator stack.";
+    private String placedSellchest = "{prefix} &7Du placerede en &esælgekiste&7.";
+    private String addedToStack = "{prefix} &7Du tilføjede en generator til stacken. ({amount} i alt)";
+    private String placedGenerator = "{prefix} &7Du placerede en &etier {tier} generator&7.";
+    private String maxGenerators = "{prefix} &7Du har ikke plads til flere generators.";
+    private String sellSuccess = "{prefix} &7Du indsamlet &a${money} &7& &b{xp} XP&7.";
+    private String sellFail = "{prefix} &7Der er ikke noget i denne sælgekiste.";
+    private String upgradeSuccess = "{prefix} &7Du opgraderet &e{amount}x tier {tier} generator&7 for &a${money}&7.";
+    private String upgradeFail = "{prefix} &7Du mangler &c${money}&7.";
+    private String upgradeMax = "{prefix} &7Du har den bedste generator tier.";
+    private String notYourGenerator = "{prefix} &7Du ejer ikke denne generator. ({owner})";
+    private String removeFromStack = "{prefix} &7Du fjernede &e{amount}x generator &7fra stacken. &7({gens} i alt)";
+    private String notEnoughInventorySpace = "{prefix} &7Du har ikke nok inventory plads.";
+    private String removeStack = "{prefix} &7Du fjernede en generator stack.";
 }
